e427b900a1bd8a3446de712cd2a3a8ba13bab48a
Scott Henderson <schenderson@gmail.com>
2016-06-12 12:47:24 -0700

    initial commit

---
+++ diy.js
@@ -0,0 +1,67 @@
+'use strict'
+
+var mysql = require('mysql')
+
+var wrappers = {
+
+  processExport: function (options, callback) {
+
+  },
+
+  processMySQLImport: function (options, callback) {
+    var host, user, database, password, query
+
+    if (!options) {
+      return callback(new Error('!options'))
+    }
+
+    if (!options.connection || !options.connection.unEncrypted || !options.connection.encrypted) {
+      return callback(new Error('!options.connection || !options.connection.unEncrypted || !options.connection.encrypted'))
+    }
+    host = options.connection.unEncrypted.host
+    user = options.connection.unEncrypted.user
+    database = options.connection.unEncrypted.database
+    password = options.connection.encrypted.password
+
+    if (!options.configuration || !options.configuration.query) {
+      return callback(new Error('!options.configuration || !options.configuration.query'))
+    }
+    query = options.configuration.query
+
+    var connection = mysql.createConnection({
+      host: host,
+      user: user,
+      password: password,
+      database: database
+    })
+
+    if (!Array.isArray(options.postMapData) || options.postMapData.length < 1) {
+      return callback(new Error('!Array.isArray(options.postMapData) || options.postMapData.length < 1'))
+    }
+
+    connection.connect(function (err) {
+      if (err) {
+        return callback(err)
+      }
+      var query = connection.query(query, options.postMapData, function (err, results) {
+        if (err) {
+          return callback(err)
+        }
+        if (results.length !== options.postMapData.length) {
+          return callback(new Error('results.length !== options.postMapData.length'))
+        }
+        var toReturn = []
+        for (var i = 0; i < results.length; i++) {
+          toReturn.push({statusCode: 200, id: results[i].insertId})
+        }
+        return callback(null, toReturn)
+      })
+    })
+  }
+}
+
+function getMySQLConnection (options) {
+
+}
+
+exports.wrappers = wrappers
---
+++ server.js
@@ -0,0 +1,35 @@
+var logger = require('winston')
+var WinstonDailyRotateFile = require('winston-daily-rotate-file')
+var expressIntegratorExtension = require('express-integrator-extension')
+var diy = require('./diy')
+
+var consoleTransportOpts = {
+  colorize: true,
+  timestamp: true,
+  prettyPrint: true
+}
+
+var fileTransportOpts = {
+  filename: './server.log',
+  maxsize: 10000000,
+  maxFiles: 2,
+  json: false,
+  handleExceptions: true
+}
+
+logger.remove(logger.transports.Console)
+logger.add(logger.transports.Console, consoleTransportOpts)
+logger.add(WinstonDailyRotateFile, fileTransportOpts)
+
+var options = {
+  port: 80,
+  systemToken: process.env.INTEGRATOR_EXTENSION_SYSTEM_TOKEN,
+  diy: diy
+}
+
+expressIntegratorExtension.createServer(options, function (err) {
+  if (err) {
+    logger.error('Failed to create express integrator extension server: ', err.message)
+    process.exit(1)
+  }
+})
