bb2d44b777a912ffbcdeaa0873935f26ba54392e
Adarsh Gupta <adarshg.nitj@gmail.com>
2016-08-08 18:09:53 +0530

    ...

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-var logger = require('winston')
-var WinstonDailyRotateFile = require('winston-daily-rotate-file')
-var expressIntegratorExtension = require('express-integrator-extension')
-var diy = require('./diy')
+var logger = require('winston');
+var WinstonDailyRotateFile = require('winston-daily-rotate-file');
+var expressIntegratorExtension = require('integrator-extension').express;
+var diy = require('./diy');
+var process = require('process');
 
 var consoleTransportOpts = {
-  colorize: true,
-  timestamp: true,
-  prettyPrint: true
-}
+    colorize: true,
+    timestamp: true,
+    prettyPrint: true
+};
 
 var fileTransportOpts = {
-  filename: './server.log',
-  maxsize: 10000000,
-  maxFiles: 2,
-  json: false,
-  handleExceptions: true
-}
+    filename: './server.log',
+    maxsize: 10000000,
+    maxFiles: 2,
+    json: false,
+    handleExceptions: true
+};
 
-logger.remove(logger.transports.Console)
-logger.add(logger.transports.Console, consoleTransportOpts)
-logger.add(WinstonDailyRotateFile, fileTransportOpts)
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, consoleTransportOpts);
+logger.add(WinstonDailyRotateFile, fileTransportOpts);
 
 var options = {
-  port: 80,
-  systemToken: process.env.INTEGRATOR_EXTENSION_SYSTEM_TOKEN,
-  diy: diy
-}
+    port: 80,
+    systemToken: process.env.INTEGRATOR_EXTENSION_SYSTEM_TOKEN,
+    diy: diy
+};
 
 expressIntegratorExtension.createServer(options, function (err) {
-  if (err) {
-    logger.error('Failed to create express integrator extension server: ', err.message)
-    process.exit(1)
-  }
-})
+    if (err) {
+        logger.error('Failed to create express integrator extension server: ', err.message);
+        process.exit(1);
+    }
+});
